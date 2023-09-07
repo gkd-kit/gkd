67de6922099fd52973e15cf3557c91ba496e3e17
lisonge <i@songe.li>
2023-09-07 21:32:32 +0800

    pref: 限制快照节点数量

--- app/src/main/java/li/songe/gkd/data/NodeInfo.kt
+++ app/src/main/java/li/songe/gkd/data/NodeInfo.kt
@@ -1,7 +1,10 @@
 package li.songe.gkd.data
 
 import android.view.accessibility.AccessibilityNodeInfo
+import com.blankj.utilcode.util.LogUtils
+import com.blankj.utilcode.util.ToastUtils
 import kotlinx.serialization.Serializable
+import li.songe.gkd.service.activityIdFlow
 import li.songe.gkd.service.forEachIndexed
 import java.util.ArrayDeque
 
@@ -11,7 +14,7 @@ data class NodeInfo(
     /**
      * null: when getChild(i) return null
      */
-    val attr: AttrInfo?
+    val attr: AttrInfo?,
 ) {
     companion object {
         fun abNodeToNode(
@@ -36,6 +39,15 @@ data class NodeInfo(
                 top.second?.forEachIndexed { index, childNode ->
                     stack.push(list.size to childNode)
                     list.add(abNodeToNode(childNode, list.size, top.first, index))
+                }
+                if (list.size > 50000) {
+//                    例子: 汽车之家, 一个界面 110000 节点, 快照文件 30 MB
+                    // Failed to allocate a 245237304 byte allocation with 100663296 free bytes and 106MB until OOM
+                    ToastUtils.showShort("节点数量至多保留50000,丢弃后续节点")
+                    LogUtils.w(
+                        nodeInfo.packageName, activityIdFlow.value, "节点数量过多"
+                    )
+                    break
                 }
             }
             return list
