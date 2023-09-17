592415edff507ad2f13884c4e1bb5be73229ee4c
lisonge <i@songe.li>
2023-09-17 17:24:52 +0800

    fix: 移除按键快照 (#26)

--- app/src/main/java/li/songe/gkd/util/Store.kt
+++ app/src/main/java/li/songe/gkd/util/Store.kt
@@ -78,7 +78,7 @@ data class Store(
     val captureScreenshot: Boolean = false,
     val httpServerPort: Int = 8888,
     val updateSubsInterval: Long = 60 * 60_000,
-    val captureVolumeKey: Boolean = false,
+    @Deprecated("https://github.com/gkd-kit/gkd/issues/26") val captureVolumeKey: Boolean = false,
     val autoCheckAppUpdate: Boolean = true,
     val toastWhenClick: Boolean = true,
     val clickToast: String = "跳过",
