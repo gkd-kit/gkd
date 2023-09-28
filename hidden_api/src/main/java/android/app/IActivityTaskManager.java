1041a3bc76aa3362943da49631dd53e83089000f
lisonge <i@songe.li>
2023-09-28 15:38:27 +0800

    fix(shizuku): 适配不同机型的隐藏api

--- hidden_api/src/main/java/android/app/IActivityTaskManager.java
+++ hidden_api/src/main/java/android/app/IActivityTaskManager.java
@@ -8,8 +8,11 @@ import java.util.List;
 
 @SuppressWarnings("unused")
 public interface IActivityTaskManager extends IInterface {
-    //    miui
+    //    XIAOMI
     List<ActivityManager.RunningTaskInfo> getTasks(int maxNum, boolean filterOnlyVisibleRecents, boolean keepIntentExtra);
+
+    // https://github.com/gkd-kit/gkd/issues/58#issuecomment-1736843795
+    List<ActivityManager.RunningTaskInfo> getTasks(int maxNum, boolean filterOnlyVisibleRecents, boolean keepIntentExtra, int displayId);
 
     // https://github.com/gkd-kit/gkd/issues/58#issuecomment-1732245703
     List<ActivityManager.RunningTaskInfo> getTasks(int maxNum);
