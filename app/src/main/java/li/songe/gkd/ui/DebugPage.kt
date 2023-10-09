890ae3080ab4a2abb19532c447a57bb30fbfc2fd
lisonge <i@songe.li>
2023-10-09 21:50:58 +0800

    fix: 高级设置页面不滑动 (#127)

--- app/src/main/java/li/songe/gkd/ui/DebugPage.kt
+++ app/src/main/java/li/songe/gkd/ui/DebugPage.kt
@@ -9,11 +9,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
@@ -85,6 +88,8 @@ fun DebugPage() {
     }, content = { contentPadding ->
         Column(
             modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
                 .padding(0.dp, 10.dp)
                 .padding(contentPadding),
             verticalArrangement = Arrangement.spacedBy(10.dp),
