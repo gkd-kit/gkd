b84e2439dfcd771bcf04d39e45a3458202762ddc
lisonge <i@songe.li>
2023-10-25 21:56:28 +0800

    perf: 快照优化

--- app/src/main/java/li/songe/gkd/util/Constants.kt
+++ app/src/main/java/li/songe/gkd/util/Constants.kt
@@ -7,6 +7,8 @@ const val FILE_UPLOAD_URL = "https://github-upload-assets.lisonge.workers.dev/"
 const val DEFAULT_SUBS_UPDATE_URL =
     "https://registry.npmmirror.com/@gkd-kit/subscription/latest/files"
 
+const val IMPORT_BASE_URL = "https://i.gkd.li/import/"
+
 val safeRemoteBaseUrls = arrayOf(
     "https://registry.npmmirror.com/@gkd-kit/",
     "https://cdn.jsdelivr.net/npm/@gkd-kit/",
