cfd4a148567d75f4f50f78d1dd68e2291c62879d
lisonge <i@songe.li>
2023-11-10 14:52:39 +0800

    feat: 使用 gkd.li

--- app/src/main/java/li/songe/gkd/util/Constants.kt
+++ app/src/main/java/li/songe/gkd/util/Constants.kt
@@ -2,13 +2,11 @@ package li.songe.gkd.util
 
 const val VOLUME_CHANGED_ACTION = "android.media.VOLUME_CHANGED_ACTION"
 
-const val FILE_UPLOAD_URL = "https://github-upload-assets.lisonge.workers.dev/"
+const val FILE_UPLOAD_URL = "https://u.gkd.li/"
+const val IMPORT_BASE_URL = "https://i.gkd.li/import/"
 
 const val DEFAULT_SUBS_UPDATE_URL =
     "https://registry.npmmirror.com/@gkd-kit/subscription/latest/files"
-
-const val IMPORT_BASE_URL = "https://i.gkd.li/import/"
-
 const val UPDATE_URL = "https://registry.npmmirror.com/@gkd-kit/app/latest/files/index.json"
 
 val safeRemoteBaseUrls = arrayOf(
