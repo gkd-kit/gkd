e16844aade89e48e1bb38f862baf3fa6986f91d0
lisonge <i@songe.li>
2023-11-22 17:14:56 +0800

    feat: 更换路由动画

--- app/src/main/java/li/songe/gkd/util/ProfileTransitions.kt
+++ app/src/main/java/li/songe/gkd/util/ProfileTransitions.kt
@@ -4,37 +4,26 @@ import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.navigation.NavBackStackEntry
-import com.blankj.utilcode.util.ScreenUtils
 import com.ramcosta.composedestinations.spec.DestinationStyle
 
 object ProfileTransitions : DestinationStyle.Animated {
-    private const val durationMillis = 400
     override fun AnimatedContentTransitionScope<NavBackStackEntry>.enterTransition(): EnterTransition? {
-        return slideInHorizontally(
-            initialOffsetX = { ScreenUtils.getScreenWidth() }, animationSpec = tween(durationMillis)
-        )
+        return slideInHorizontally(tween()) { it }
     }
 
     override fun AnimatedContentTransitionScope<NavBackStackEntry>.exitTransition(): ExitTransition? {
-        return slideOutHorizontally(
-            targetOffsetX = { -ScreenUtils.getScreenWidth() / 2 },
-            animationSpec = tween(durationMillis)
-        )
+        return slideOutHorizontally(tween()) { -it } + fadeOut(tween())
     }
 
     override fun AnimatedContentTransitionScope<NavBackStackEntry>.popEnterTransition(): EnterTransition? {
-        return slideInHorizontally(
-            initialOffsetX = { -ScreenUtils.getScreenWidth() / 2 },
-            animationSpec = tween(durationMillis)
-        )
+        return slideInHorizontally(tween()) { -it }
     }
 
     override fun AnimatedContentTransitionScope<NavBackStackEntry>.popExitTransition(): ExitTransition? {
-        return slideOutHorizontally(
-            targetOffsetX = { ScreenUtils.getScreenWidth() }, animationSpec = tween(durationMillis)
-        )
+        return slideOutHorizontally(tween()) { it }
     }
 }
