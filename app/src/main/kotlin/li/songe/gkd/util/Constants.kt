c0011a9e7e0d23825e76f2fa8fb734a12df7bdf6
lisonge <i@songe.li>
2023-12-04 00:07:36 +0800

    perf: 服务器主页自动连接到设备

--- app/src/main/kotlin/li/songe/gkd/util/Constants.kt
+++ app/src/main/kotlin/li/songe/gkd/util/Constants.kt
@@ -9,6 +9,9 @@ const val DEFAULT_SUBS_UPDATE_URL =
     "https://registry.npmmirror.com/@gkd-kit/subscription/latest/files"
 const val UPDATE_URL = "https://registry.npmmirror.com/@gkd-kit/app/latest/files/index.json"
 
+const val SERVER_SCRIPT_URL =
+    "https://registry-direct.npmmirror.com/@gkd-kit/config/latest/files/dist/server.js"
+
 const val REPOSITORY_URL = "https://github.com/gkd-kit/gkd"
 
 val safeRemoteBaseUrls = arrayOf(
