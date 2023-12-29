e64956067dc1f5a0a438505f46f1f495311672b5
lisonge <i@songe.li>
2023-12-29 20:13:45 +0800

    perf: update dependency

--- hidden_api/build.gradle.kts
+++ hidden_api/build.gradle.kts
@@ -9,7 +9,6 @@ android {
 
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
     }
 
     buildTypes {
