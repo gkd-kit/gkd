bfb8dab2f9086290336dc4d7d23ec043c860cd4e
lisonge <i@songe.li>
2024-01-20 12:37:10 +0800

    fix: get versionName be null (#491)

--- app/src/main/kotlin/li/songe/gkd/data/AppInfo.kt
+++ app/src/main/kotlin/li/songe/gkd/data/AppInfo.kt
@@ -5,8 +5,8 @@ import android.graphics.drawable.Drawable
 data class AppInfo(
     val id: String,
     val name: String,
-    val icon: Drawable,
+    val icon: Drawable?,
     val versionCode: Int,
-    val versionName: String,
+    val versionName: String?,
     val isSystem: Boolean,
 )
