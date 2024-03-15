a4fa10911c4f4d9b539577d99439da2ddc319054
lisonge <i@songe.li>
2024-03-15 20:54:56 +0800

    chore: v1.7.0-beta.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
         targetSdk = libs.versions.android.targetSdk.get().toInt()
 
         applicationId = "li.songe.gkd"
-        versionCode = 22
-        versionName = "1.7.0-beta.4"
+        versionCode = 23
+        versionName = "1.7.0-beta.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
