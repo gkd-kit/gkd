6ba1f4af987a0c876cd00bfca2da7a25ccc216ae
lisonge <i@songe.li>
2024-03-25 00:30:12 +0800

    fix: floatingActionButton 80.dp (#572)

--- app/src/main/kotlin/li/songe/gkd/ui/home/SubsManagePage.kt
+++ app/src/main/kotlin/li/songe/gkd/ui/home/SubsManagePage.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -329,6 +331,9 @@ fun useSubsManagePage(): ScaffoldExt {
                             )
                         }
                     }
+                }
+                item {
+                    Spacer(modifier = Modifier.height(80.dp))
                 }
             }
             PullRefreshIndicator(
