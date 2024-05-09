77643161aa7ee21d0147b7ca754027a05f403364
lisonge <i@songe.li>
2024-05-09 21:30:36 +0800

    perf: short log file href

--- app/src/main/kotlin/li/songe/gkd/data/GithubPoliciesAsset.kt
+++ app/src/main/kotlin/li/songe/gkd/data/GithubPoliciesAsset.kt
@@ -1,9 +1,13 @@
 package li.songe.gkd.data
 
 import kotlinx.serialization.Serializable
+import li.songe.gkd.util.FILE_SHORT_URL
 
 @Serializable
 data class GithubPoliciesAsset(
     val id: Int,
     val href: String,
-)
+) {
+    val shortHref: String
+        get() = FILE_SHORT_URL + id
+}
