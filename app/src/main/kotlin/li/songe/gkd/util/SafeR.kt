b1ae97f211de320dc48a8d4f0c23f97beffe12f9
lisonge <i@songe.li>
2024-05-13 12:32:38 +0800

    perf: safe R

---
+++ app/src/main/kotlin/li/songe/gkd/util/SafeR.kt
@@ -0,0 +1,12 @@
+package li.songe.gkd.util
+
+import li.songe.gkd.R
+
+/**
+ * ![image](https://github.com/gkd-kit/gkd/assets/38517192/c9325110-d90f-4041-a01d-404d14c5d34d)
+ */
+@Suppress("UNRESOLVED_REFERENCE")
+object SafeR {
+    val app_name: Int = R.string.app_name
+    val ic_status: Int = R.drawable.ic_status
+}
