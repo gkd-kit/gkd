5e02db6c75695bb860e7d330278593014c92b47f
lisonge <i@songe.li>
2024-05-24 21:23:13 +0800

    perf: gradle api

--- selector/build.gradle.kts
+++ selector/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kotlin.serialization)
@@ -5,8 +7,8 @@ plugins {
 
 kotlin {
     jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = JavaVersion.VERSION_17.majorVersion
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
         }
     }
 //    https://kotlinlang.org/docs/js-to-kotlin-interop.html#kotlin-types-in-javascript
@@ -20,12 +22,16 @@ kotlin {
         all {
             languageSettings.optIn("kotlin.js.ExperimentalJsExport")
         }
-    }
-    sourceSets["commonMain"].dependencies {
-        implementation(libs.kotlin.stdlib.common)
-    }
-    sourceSets["jvmTest"].dependencies {
-        implementation(libs.kotlinx.serialization.json)
-        implementation(libs.junit)
+        commonMain {
+            dependencies {
+                implementation(libs.kotlin.stdlib.common)
+            }
+        }
+        jvmTest {
+            dependencies {
+                implementation(libs.kotlinx.serialization.json)
+                implementation(libs.junit)
+            }
+        }
     }
 }
