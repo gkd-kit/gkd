6dc9fb5b208a240e596d226a5a7e8c17b7d97b31
lisonge <i@songe.li>
2024-06-09 05:13:09 +0800

    perf: update rule data in default thread

--- app/src/main/kotlin/li/songe/gkd/util/SubsState.kt
+++ app/src/main/kotlin/li/songe/gkd/util/SubsState.kt
@@ -15,6 +15,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
@@ -292,7 +293,7 @@ val ruleSummaryFlow by lazy {
             appIdToAllGroups = appAllGroups.mapValues { e -> e.value.toImmutableList() }
                 .toImmutableMap()
         )
-    }.stateIn(appScope, SharingStarted.Eagerly, RuleSummary())
+    }.flowOn(Dispatchers.Default).stateIn(appScope, SharingStarted.Eagerly, RuleSummary())
 }
 
 private fun loadSubs(id: Long): RawSubscription {
