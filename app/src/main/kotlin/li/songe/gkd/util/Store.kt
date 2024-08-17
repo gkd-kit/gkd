8e3246a9a639e4c4f2a9418d2a53d3eefdcb44b0
lisonge <i@songe.li>
2024-08-17 20:00:21 +0800

    feat: update channel

--- app/src/main/kotlin/li/songe/gkd/util/Store.kt
+++ app/src/main/kotlin/li/songe/gkd/util/Store.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
-import li.songe.gkd.BuildConfig.ENABLED_UPDATE
+import li.songe.gkd.BuildConfig
 import li.songe.gkd.appScope
 
 private inline fun <reified T> createStorageFlow(
@@ -48,7 +48,7 @@ data class Store(
     val httpServerPort: Int = 8888,
     val updateSubsInterval: Long = UpdateTimeOption.Everyday.value,
     val captureVolumeChange: Boolean = false,
-    val autoCheckAppUpdate: Boolean = ENABLED_UPDATE,
+    val autoCheckAppUpdate: Boolean = BuildConfig.ENABLED_UPDATE,
     val toastWhenClick: Boolean = true,
     val clickToast: String = "GKD",
     val autoClearMemorySubs: Boolean = true,
@@ -67,6 +67,7 @@ data class Store(
     val useCustomNotifText: Boolean = false,
     val customNotifText: String = "\${i}全局/\${k}应用/\${u}规则组/\${n}触发",
     val enableActivityLog: Boolean = false,
+    val updateChannel: Int = if (BuildConfig.VERSION_NAME.contains("beta")) UpdateChannelOption.Beta.value else UpdateChannelOption.Stable.value,
 )
 
 val storeFlow by lazy {
