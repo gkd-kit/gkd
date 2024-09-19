bcbffbdd89c3d4fbd45770afb086980cc37d9b55
lisonge <i@songe.li>
2024-09-19 17:27:42 +0800

    chore: test

--- app/src/test/kotlin/li/songe/gkd/ExampleUnitTest.kt
+++ app/src/test/kotlin/li/songe/gkd/ExampleUnitTest.kt
@@ -2,16 +2,10 @@ package li.songe.gkd
 
 import org.junit.Test
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
 
     @Test
-    fun check_parser_selector() {
-
+    fun test() {
     }
 
 }
