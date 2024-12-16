250561580ac627c67897bd34cc1eac6104f1b9fa
二刺螈 <i@songe.li>
2024-12-16 21:20:16 +0800

    feat: add qsTile intent (#823)

--- app/src/main/kotlin/li/songe/gkd/ui/home/HomePage.kt
+++ app/src/main/kotlin/li/songe/gkd/ui/home/HomePage.kt
@@ -1,5 +1,9 @@
 package li.songe.gkd.ui.home
 
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Build
+import android.service.quicksettings.TileService
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -25,8 +29,12 @@ import kotlinx.coroutines.delay
 import li.songe.gkd.MainActivity
 import li.songe.gkd.OpenFileActivity
 import li.songe.gkd.data.importData
+import li.songe.gkd.debug.FloatingTileService
+import li.songe.gkd.debug.HttpTileService
+import li.songe.gkd.debug.SnapshotTileService
 import li.songe.gkd.util.LocalNavController
 import li.songe.gkd.util.ProfileTransitions
+import li.songe.gkd.util.componentName
 import li.songe.gkd.util.launchTry
 import li.songe.gkd.util.toast
 
@@ -56,15 +64,8 @@ fun HomePage() {
         val intent = context.intent ?: return@LaunchedEffect
         context.intent = null
         LogUtils.d(intent)
-        val uri = intent.data?.normalizeScheme() ?: return@LaunchedEffect
-        val source = intent.getStringExtra("source")
-        if (source == OpenFileActivity::class.qualifiedName) {
-            vm.viewModelScope.launchTry(Dispatchers.IO) {
-                toast("加载导入...")
-                vm.tabFlow.value = subsPage.navItem
-                importData(uri)
-            }
-        } else if (uri.scheme == "gkd" && uri.host == "page") {
+        val uri = intent.data?.normalizeScheme()
+        if (uri != null && uri.scheme == "gkd" && uri.host == "page") {
             delay(200)
             when (uri.path) {
                 "/1" -> {
@@ -74,6 +75,25 @@ fun HomePage() {
                 "/2" -> {
                     navController.toDestinationsNavigator().navigate(SnapshotPageDestination)
                 }
+            }
+        } else if (uri != null && intent.getStringExtra("source") == OpenFileActivity::class.qualifiedName) {
+            vm.viewModelScope.launchTry(Dispatchers.IO) {
+                toast("加载导入...")
+                vm.tabFlow.value = subsPage.navItem
+                importData(uri)
+            }
+        } else if (intent.action == TileService.ACTION_QS_TILE_PREFERENCES) {
+            val qsTileCpt = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                intent.getParcelableExtra(Intent.EXTRA_COMPONENT_NAME, ComponentName::class.java)
+            } else {
+                @Suppress("DEPRECATION")
+                intent.getParcelableExtra(Intent.EXTRA_COMPONENT_NAME) as ComponentName?
+            } ?: return@LaunchedEffect
+            delay(200)
+            if (qsTileCpt == HttpTileService::class.componentName || qsTileCpt == FloatingTileService::class.componentName) {
+                navController.toDestinationsNavigator().navigate(AdvancedPageDestination)
+            } else if (qsTileCpt == SnapshotTileService::class.componentName) {
+                navController.toDestinationsNavigator().navigate(SnapshotPageDestination)
             }
         }
     })
