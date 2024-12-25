b29fd1a274d3986d8eb1c97f8acd8b0984a6676d
二刺螈 <i@songe.li>
2024-12-25 21:55:42 +0800

    fix: showActivityId

--- app/src/main/kotlin/li/songe/gkd/util/Others.kt
+++ app/src/main/kotlin/li/songe/gkd/util/Others.kt
@@ -28,3 +28,15 @@ fun Bitmap.isEmptyBitmap(): Boolean {
 }
 
 class InterruptRuleMatchException() : Exception()
+
+fun getShowActivityId(appId: String, activityId: String?) {
+    if (activityId != null) {
+        if (activityId.startsWith(appId) && activityId.getOrNull(appId.length) == '.') {
+            activityId.substring(appId.length)
+        } else {
+            activityId
+        }
+    } else {
+        null
+    }
+}
