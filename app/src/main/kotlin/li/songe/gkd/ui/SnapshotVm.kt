5b4eda3ce08bb09cfdb295623c7ffcd211735237
二刺螈 <i@songe.li>
2025-03-23 18:50:27 +0800

    perf: SnapshotPage firstLoading

--- app/src/main/kotlin/li/songe/gkd/ui/SnapshotVm.kt
+++ app/src/main/kotlin/li/songe/gkd/ui/SnapshotVm.kt
@@ -5,8 +5,10 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.stateIn
 import li.songe.gkd.db.DbSet
+import li.songe.gkd.util.LinkLoad
 
 class SnapshotVm : ViewModel() {
-    val snapshotsState = DbSet.snapshotDao.query()
+    val linkLoad = LinkLoad(viewModelScope)
+    val snapshotsState = DbSet.snapshotDao.query().let(linkLoad::invoke)
         .stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
 }
