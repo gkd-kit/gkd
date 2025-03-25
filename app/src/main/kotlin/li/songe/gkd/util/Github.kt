3d1d2a6e7ce556e14784b817aa2ed4efd213944e
二刺螈 <i@songe.li>
2025-03-25 12:30:54 +0800

    perf: update github upload

--- app/src/main/kotlin/li/songe/gkd/util/Github.kt
+++ app/src/main/kotlin/li/songe/gkd/util/Github.kt
@@ -28,25 +28,35 @@ import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.request.put
 import io.ktor.client.request.setBody
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.Headers
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMessageBuilder
 import io.ktor.http.HttpStatusCode
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.jsonObject
 import li.songe.gkd.data.GithubPoliciesAsset
 import li.songe.gkd.ui.component.autoFocus
+import li.songe.json5.Json5
 import java.io.File
 
 private fun HttpMessageBuilder.setCommonHeaders(cookie: String) {
     header("Cookie", cookie)
-    header("Referer", "https://github.com/")
+    header("Referer", "https://github.com/gkd-kit/inspect/issues/46")
     header("Origin", "https://github.com")
     header(
         "User-Agent",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0"
     )
+}
+
+private fun String.json5ToJsonString(): String {
+    return json.encodeToString(Json5.parseToJson5Element(this))
 }
 
 @Suppress("PropertyName")
@@ -60,6 +70,19 @@ private data class UploadPoliciesAssetsResponse(
 )
 
 data class GithubCookieException(override val message: String) : Exception(message)
+
+private suspend fun graphqlFetch(
+    cookie: String,
+    data: String,
+): HttpResponse {
+    return client.post("https://github.com/_graphql") {
+        setCommonHeaders(cookie)
+        header("Accept", "application/json")
+        header("Content-Type", "text/plain;charset=UTF-8")
+        header("GitHub-Verified-Fetch", "true")
+        setBody(data)
+    }
+}
 
 // https://github.com/lisonge/user-attachments
 suspend fun uploadFileToGithub(
@@ -111,6 +134,60 @@ suspend fun uploadFileToGithub(
         }))
     }
 
+    // send file url text to github comment
+    val commentResultResp = graphqlFetch(
+        cookie,
+        """
+        {
+            query: '50e7774b5a519b88858e02e46e0348da',
+            variables: {
+              connections: [
+                'client:I_kwDOJ3SWBc6viUWN:__Issue__backTimelineItems_connection(visibleEventsOnly:true)',
+              ],
+              input: {
+                body: '${policiesResp.asset.href}',
+                subjectId: 'I_kwDOJ3SWBc6viUWN',
+              },
+            },
+        }
+        """.json5ToJsonString()
+    )
+    val commentResult = json.decodeFromString<JsonElement>(commentResultResp.bodyAsText())
+    val commentId = (commentResult.jsonObject["data"]
+        ?.jsonObject["addComment"]
+        ?.jsonObject["timelineEdge"]
+        ?.jsonObject["node"]
+        ?.jsonObject["id"]?.toString() ?: error("commentId not found"))
+
+    // delay is needed
+    delay(1000)
+
+    // unsubscribe the comment
+    graphqlFetch(
+        cookie,
+        """
+        {
+            query: 'd0752b2e49295017f67c84f21bfe41a3',
+            variables: {
+                input: { state: 'UNSUBSCRIBED', subscribableId: 'I_kwDOJ3SWBc6viUWN' },
+            },
+        }
+        """.json5ToJsonString()
+    )
+
+    // delete the comment
+    graphqlFetch(
+        cookie,
+        """
+        {
+            query: 'b0f125991160e607a64d9407db9c01b3',
+            variables: {
+                connections: [],
+                input: { id: $commentId },
+            },
+        }
+        """.json5ToJsonString()
+    )
     return policiesResp.asset
 }
 
