cc1cb0865f63cdbb12db54350f388f81b01537b0
二刺螈 <i@songe.li>
2025-09-23 23:49:01 +0800

    perf: ActivityLogCard

--- app/src/main/kotlin/li/songe/gkd/ui/ActivityLogPage.kt
+++ app/src/main/kotlin/li/songe/gkd/ui/ActivityLogPage.kt
@@ -3,6 +3,7 @@ package li.songe.gkd.ui
 import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
@@ -11,8 +12,10 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
@@ -24,7 +27,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -35,6 +40,7 @@ import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemKey
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootGraph
+import com.ramcosta.composedestinations.generated.destinations.AppConfigPageDestination
 import li.songe.gkd.MainActivity
 import li.songe.gkd.data.ActivityLog
 import li.songe.gkd.db.DbSet
@@ -53,6 +59,7 @@ import li.songe.gkd.ui.share.LocalMainViewModel
 import li.songe.gkd.ui.share.noRippleClickable
 import li.songe.gkd.ui.style.EmptyHeight
 import li.songe.gkd.ui.style.ProfileTransitions
+import li.songe.gkd.ui.style.iconTextSize
 import li.songe.gkd.ui.style.itemHorizontalPadding
 import li.songe.gkd.ui.style.scaffoldPadding
 import li.songe.gkd.util.appInfoMapFlow
@@ -118,7 +125,7 @@ fun ActivityLogPage() {
                 CompositionLocalProvider(
                     LocalNumberCharWidth provides timeTextWidth
                 ) {
-                    ActivityLogCard(i = i, actionLog = actionLog, lastActionLog = lastActionLog)
+                    ActivityLogCard(i = i, activityLog = actionLog, lastActivityLog = lastActionLog)
                 }
             }
             item(ListPlaceholder.KEY, ListPlaceholder.TYPE) {
@@ -134,38 +141,68 @@ fun ActivityLogPage() {
 @Composable
 private fun ActivityLogCard(
     i: Int,
-    actionLog: ActivityLog,
-    lastActionLog: ActivityLog?,
+    activityLog: ActivityLog,
+    lastActivityLog: ActivityLog?,
 ) {
     val mainVm = LocalMainViewModel.current
-    val isDiffApp = actionLog.appId != lastActionLog?.appId
+    val isDiffApp = activityLog.appId != lastActivityLog?.appId
     val verticalPadding = if (i == 0) 0.dp else if (isDiffApp) 12.dp else 8.dp
-    val showActivityId = actionLog.showActivityId
+    val showActivityId = activityLog.showActivityId
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(
-                start = itemHorizontalPadding,
-                end = itemHorizontalPadding,
+                start = itemHorizontalPadding / 2,
+                end = itemHorizontalPadding / 2,
                 top = verticalPadding
             )
     ) {
         if (isDiffApp) {
-            AppNameText(appId = actionLog.appId)
+            Row(
+                modifier = Modifier
+                    .padding(start = itemHorizontalPadding / 4)
+                    .clip(MaterialTheme.shapes.extraSmall)
+                    .clickable(onClick = throttle {
+                        mainVm.navigatePage(
+                            AppConfigPageDestination(
+                                appId = activityLog.appId,
+                            )
+                        )
+                    })
+                    .fillMaxWidth()
+                    .padding(start = 5.dp),
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                CompositionLocalProvider(LocalContentColor provides MaterialTheme.colorScheme.primary) {
+                    Spacer(
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .background(MaterialTheme.colorScheme.secondary)
+                            .size(4.dp)
+                    )
+                    AppNameText(appId = activityLog.appId, modifier = Modifier.weight(1f))
+                    PerfIcon(
+                        imageVector = PerfIcon.KeyboardArrowRight,
+                        modifier = Modifier
+                            .iconTextSize()
+                    )
+                }
+            }
         }
         Row(
             modifier = Modifier
-                .clickable(
-                    onClick = {
-                        mainVm.textFlow.value = listOfNotNull(
-                            appInfoMapFlow.value[actionLog.appId]?.name,
-                            actionLog.appId,
-                            actionLog.showActivityId,
-                        ).joinToString("\n")
-                    },
-                )
+                .padding(start = itemHorizontalPadding / 4)
+                .clickable(onClick = {
+                    mainVm.textFlow.value = listOfNotNull(
+                        appInfoMapFlow.value[activityLog.appId]?.name,
+                        activityLog.appId,
+                        activityLog.showActivityId,
+                    ).joinToString("\n")
+                })
                 .fillMaxWidth()
                 .height(IntrinsicSize.Min)
+                .padding(start = itemHorizontalPadding / 4)
         ) {
             Spacer(modifier = Modifier.width(2.dp))
             Spacer(
@@ -179,7 +216,7 @@ private fun ActivityLogCard(
                 modifier = Modifier.weight(1f)
             ) {
                 FixedTimeText(
-                    text = actionLog.date,
+                    text = activityLog.date,
                     style = MaterialTheme.typography.bodySmall,
                     color = MaterialTheme.colorScheme.secondary,
                 )
