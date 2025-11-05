a0a6b5e49f8405b0ce5e5a5a32ad16becab6962a
二刺螈 <i@songe.li>
2025-11-05 18:16:08 +0800

    chore: v1.11.0-beta.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,8 +66,8 @@ android {
         targetSdk = rootProject.ext["android.targetSdk"] as Int
 
         applicationId = "li.songe.gkd"
-        versionCode = 72
-        versionName = "1.11.0-beta.6"
+        versionCode = 73
+        versionName = "1.11.0-beta.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
